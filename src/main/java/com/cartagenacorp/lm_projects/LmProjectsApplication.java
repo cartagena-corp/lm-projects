e2b2c3a31b22807b03fee83650d9c7005c70a159
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-08-09 09:47:07 -0500

    build: Generatin .war File instead of .jar

--- src/main/java/com/cartagenacorp/lm_projects/LmProjectsApplication.java
+++ src/main/java/com/cartagenacorp/lm_projects/LmProjectsApplication.java
@@ -2,12 +2,19 @@ package com.cartagenacorp.lm_projects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmProjectsApplication {
+public class LmProjectsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmProjectsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmProjectsApplication.class);
 	}
 
 }
